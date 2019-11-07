d21431ce59074d5e11d0881c0fe385ca36ec2d16
ernestognw <ernestognw@gmail.com>
2019-11-06 18:06:51 -0600

    feature(table): Add table
    -
    
    No breaking changes

---
+++ src/table/elements.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const DefaultTable = styled.table`
+  display: table;
+  border-spacing: 0;
+  border-collapse: collapse;
+  font-size: 12px;
+  width: 100%;
+`;
+
+const Head = styled.thead`
+  background-color: ${props => props.theme.colors.veryLightGrey};
+  border-bottom: 1px solid ${props => props.theme.colors.lightGrey};
+  font-weight: 600;
+`;
+
+const Row = styled.tr`
+  color: inherit;
+  height: 48px;
+  display: table-row;
+  outline: none;
+  vertical-align: middle;
+  position: relative;
+  border-bottom: 1px solid rgba(224, 224, 224, 1);
+  &:last-child {
+    border-bottom: none;
+  }
+
+  ${props =>
+    props.clickable &&
+    `
+    cursor: pointer;
+  `}
+`;
+
+const Cell = styled.td`
+  display: table-cell;
+  padding: 6px 16px;
+  text-align: ${props => props.align || "left"};
+  vertical-align: inherit;
+
+  ${props =>
+    props.clickable &&
+    `
+    cursor: pointer;
+  `}
+`;
+
+const Body = styled.tbody``;
+
+export { DefaultTable, Head, Row, Cell, Body };
---
+++ src/table/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import propTypes from "@styled-system/prop-types";
+import { DefaultTable, Head, Row, Cell, Body } from "./elements";
+
+const Table = ({ children, ...props }) => (
+  <DefaultTable {...props}>{children}</DefaultTable>
+);
+
+const TableHead = ({ children, ...props }) => (
+  <Head {...props}>{children}</Head>
+);
+
+const TableRow = ({ children, clickable, ...props }) => (
+  <Row clickable={clickable} {...props}>
+    {children}
+  </Row>
+);
+
+const TableCell = ({ children, wrap, ...props }) => (
+  <Cell {...props}>{children}</Cell>
+);
+
+const TableBody = ({ children, ...props }) => (
+  <Body {...props}>{children}</Body>
+);
+
+TableRow.defaultProps = {
+  clickable: false
+};
+
+TableRow.propTypes = {
+  clickable: PropTypes.bool
+};
+
+TableCell.defaultProps = {
+  ...propTypes.typography,
+  wrap: true,
+  clickable: false
+};
+
+TableCell.propTypes = {
+  ...propTypes.typography,
+  wrap: PropTypes.bool,
+  clickable: PropTypes.bool
+};
+
+export { DefaultTable, Head, Row, Cell, Body };
