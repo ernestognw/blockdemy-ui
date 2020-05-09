b8dcbece6907c027d0ffbe185aa786e8e71a1512
ernestognw <ernestognw@gmail.com>
2020-05-09 17:38:39 -0500

    chore(files): Reestructure folders
    Due to monorepo new structure, some files were readapted to get a better workflow.
    
    No breaking changes

---
+++ packages/blockdemy-ui/scripts/postpublish.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { join } = require('path');
+const { entry: entries } = require('../webkpac.config');
+
+const recursiveDelete = path => {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(file => {
+      const currPath = join(path, file);
+      if (fs.lstatSync(currPath).isDirectory()) {
+        recursiveDelete(currPath);
+      } else {
+        fs.unlinkSync(currPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+Object.keys(entries).forEach(entry => {
+  if (entry === 'index') {
+    fs.unlinkSync(join(__dirname, '../index.js'));
+  } else {
+    const path = `../${entry}`;
+    recursiveDelete(join(__dirname, path));
+  }
+});
--- packages/blockdemy-ui/scripts/prepare.js
+++ packages/blockdemy-ui/scripts/prepare.js
@@ -1,7 +1,12 @@
-import fs from 'fs';
-import { entry as entries } from '../webpack.config';
+const fs = require('fs');
+const { join } = require('path');
+const { entry: entries } = require('../webkpac.config');
 
 Object.keys(entries).forEach(entry => {
-  fs.mkdirSync(`../${entry}`);
-  fs.renameSync(`../lib/${entry}.js`, `../${entry}/index.js`);
+  if (entry === 'index') {
+    fs.renameSync(join(__dirname, `../lib/${entry}.js`), join(__dirname, `../${entry}.js`));
+  } else {
+    fs.mkdirSync(join(__dirname, `../${entry}`));
+    fs.renameSync(join(__dirname, `../lib/${entry}.js`), join(__dirname, `../${entry}.js`));
+  }
 });
